1c74c8277008fab6104a6cbd8fdbd85c928c8449
Zhenya <wowkylgame@gmail.com>
2023-01-16 22:59:21 +0300

    task 1 complete

---
+++ exercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if (number1 > number2)
+    {
+    max = number1;
+    min = number2;
+    }
+max = number2;
+min = number1;
+System.Console.WriteLine(max + " - максимальное число");
+System.Console.WriteLine(min + " - минимальное число");
