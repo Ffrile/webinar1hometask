46707d608771f72768a3c0701749fa1e093f273a
Zhenya <wowkylgame@gmail.com>
2023-01-16 23:30:14 +0300

    task 1 corrected

--- exercise1/Program.cs
+++ exercise1/Program.cs
@@ -3,15 +3,22 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-int max = 0;
-int min = 0;
-
+if (number1 == number2)
+{
+    System.Console.WriteLine("Числа равны");
+}
+else
 if (number1 > number2)
-    {
-    max = number1;
-    min = number2;
-    }
-max = number2;
-min = number1;
-System.Console.WriteLine(max + " - максимальное число");
-System.Console.WriteLine(min + " - минимальное число");
+{
+    System.Console.WriteLine(number1 + " - max");
+    System.Console.WriteLine(number2 + " - min");
+
+}
+else
+{
+    System.Console.WriteLine(number2 + " - max");
+    System.Console.WriteLine(number1 + " - min");
+
+}
+
+
