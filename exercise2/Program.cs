748eb01f3b0513680ba145f45cd8a8578929bd3c
Zhenya <wowkylgame@gmail.com>
2023-01-19 23:09:51 +0300

    task 2 corrected

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -1,19 +1,21 @@
-﻿System.Console.WriteLine("Введите первое число:");
+﻿//  Напишите программу, которая принимает на вход три числа 
+//  и выдаёт максимальное из этих чисел.
+
+
+System.Console.WriteLine("Введите первое число:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите третье число:");
 int number3 = Convert.ToInt32(Console.ReadLine());
-
-if (number1 >= number2 & number1 >= number3)
+int max = number1;
+if (max < number2)
     {
-    System.Console.WriteLine(number1);
+    max = number2;
     }
-if (number2 >= number1 & number2 >= number3)
+if (max < number3)
    {
-    System.Console.WriteLine(number2);
+    max = number3;
    }    
-if (number3 >= number1 & number3 >= number2)
-    {
-    System.Console.WriteLine(number3);
-    }
+
+ System.Console.WriteLine($"Максимальное число {max}");
