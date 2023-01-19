09d628cc1f10ac47ccdaf77c7d38246f3a097bda
Zhenya <wowkylgame@gmail.com>
2023-01-19 23:06:47 +0300

    task 4 complete

---
+++ exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 2)
+{
+    for (int count = 2; count <= num; count = count + 2)
+    {
+        System.Console.WriteLine(count);
+    }
+}
+else
+{
+    System.Console.WriteLine("нет чисел удволетворяющих условию");
+}
